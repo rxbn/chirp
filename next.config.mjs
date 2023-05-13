c536147f86f4c08d68aa6697adc7a26fcf5e718f
Robin Scherrer <mail@robinscherrer.com>
2023-05-13 20:05:26 +0200

    fix: Use svg instead of png for verifed badge

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ await import("./src/env.mjs");
 const config = {
   reactStrictMode: true,
   images: {
-    domains: ["images.clerk.dev", "upload.wikimedia.org"],
+    domains: ["images.clerk.dev"],
   },
 
   /**
